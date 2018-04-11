59e3957b36a4729a2b47f60358460cfab5fa3f6f
Justin Lum <j.lum07@gmail.com>
2018-04-11 13:18:48 +0000

    setting up repo

---
+++ character-positioning.js
@@ -0,0 +1,16 @@
+function countLetters(inStr) {
+
+  var count = {};
+  //var str = inStr.replace(/\s/g,'');
+  var arrayStr = inStr.split("");
+
+  for (var i = 0; i < arrayStr.length; i++) {
+    count[arrayStr[i]] ? count[arrayStr[i]].push(i) : count[arrayStr[i]] = [i];
+  }
+
+  delete count[" "];
+  return count;
+}
+console.log(countLetters("          aabbcczz"));
+//console.log(countLetters("lighthouse in the house"));
+//console.log(countLetters("the quick brown fox jumped over the lazy dog"));
